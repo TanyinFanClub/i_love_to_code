541542a4b12bb83d9f0c5801226c48d39359a41f
Wang JiaJun <jiajun.wang.2018@smu.edu.sg>
2020-03-05 20:30:35 +0800

    Will return a list of medicine from port 6969

--- project/SQL/medicine.sql
+++ project/SQL/medicine.sql
@@ -3,32 +3,34 @@ use ESD_Project;
 
 drop table if exists prescription;
 
-CREATE TABLE IF NOT EXISTS prescription(
-	queueid VARCHAR(10) NOT NULL, 
-    	registration_timestamp date NOT NULL,
-    	nric VARCHAR(9) NOT NULL,
-    	medication_list VARCHAR(100) NULL,
-    	CONSTRAINT prescription_pk PRIMARY KEY (queueid,registration_timestamp),
-		CONSTRAINT prescription_fk1 foreign key(queueid) references diagnosis(queueid),
-		CONSTRAINT prescription_fk2 foreign key(registration_timestamp) references diagnosis(registration_timestamp),
-		CONSTRAINT prescription_fk3 foreign key(nric) references patient(nric)
+CREATE TABLE IF NOT EXISTS Prescription(
+	QueueID VARCHAR(10) NOT NULL, 
+    	Registration_timestamp date NOT NULL,
+    	NRIC VARCHAR(9) NOT NULL,
+    	Medication_ID VARCHAR(10) NOT NULL,
+    	CONSTRAINT prescription_pk PRIMARY KEY (QueueID,Registration_timestamp,Medication_ID),
+	CONSTRAINT prescription_fk1 foreign key(QueueID) references diagnosis(QueueID),
+	CONSTRAINT prescription_fk2 foreign key(Registration_timestamp) references diagnosis(Registration_timestamp),
+	CONSTRAINT prescription_fk3 foreign key(NRIC) references patient(NRIC),
+	CONSTRAINT prescription_fk4 foreign key(Medication_ID) references medication(Medication_ID)
 );
 
-drop table if exists medication;
+drop table if exists Medication;
 
-CREATE TABLE IF NOT EXISTS medication(
-	medicine_id VARCHAR(10) NOT NULL, 
-    	medicine_name VARCHAR(100) NOT NULL,
-    	quantity INT NOT NULL,
-    	description VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS Medication(
+	Medication_ID VARCHAR(10) NOT NULL, 
+    	Medicine_name VARCHAR(100) NOT NULL,
+    	Quantity INT NOT NULL,
+    	Description VARCHAR(100) NOT NULL,
     	CONSTRAINT medication_pk PRIMARY KEY (medicine_id)
 );
 
-INSERT INTO prescription VALUES
-('4907', '2020-03-01 12:15:00', 'S6964907Z', ?),
-('4908', '2020-03-01 12:30:00', 'S6964908Z', ?);
+INSERT INTO Prescription VALUES
+('4907', '2020-03-01 12:15:00', 'S6964907Z', 111),
+('4907', '2020-03-01 12:15:00', 'S6964907Z', 110),
+('4908', '2020-03-01 12:30:00', 'S6964908Z', 559);
 
-INSERT INTO medication VALUES
+INSERT INTO Medication VALUES
 ('111','Acetaminophen',10,'Acetaminophen is a pain reliever and a fever reducer.'),
 ('110','Citalopram',15,'Citalopram is used to treat depression.'),
 ('559','Corticosteroids',8,'Decreasing inflammation and reducing the activity of the immune system'),
---
+++ project/microservices/medication.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from os import environ
+
+app = Flask(__name__)
+#TESTING
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/esd_project' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Actual
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+# app.config['SQLALCHEMY_TRACK_MODOFICATIONS'] = False
+
+db = SQLAlchemy(app)
+CORS(app)
+
+# class Prescription(db.Model):
+#     __tablename__ = 'Prescription'
+    
+#     QueueID = db.Column(db.String(10), primary_key = True)
+#     Registration_timestamp = db.Column(db.Date, primary_key=True)
+#     NRIC = db.Column(db.String(9),nullable=False)
+#     Medication_ID = db.Column(db.String(10))
+
+#     def __init__(self, QueueID, Registration_timestamp, NRIC, Medication_ID):
+#         self.QueueID = QueueID
+#         self.Registration_timestamp = Registration_timestamp
+#         self.NRIC = NRIC
+#         self.Medication_ID = Medication_ID
+
+#     def json(self):
+#         return {"QueueID": self.QueueID,
+#                 "Registration_timestamp": self.Registration_timestamp,
+#                 "NRIC": self.NRIC,
+#                 "Medication_ID": self.Medication_ID
+#                 }
+class Medication(db.Model):
+    __tablename__ = 'Medication'
+    
+    Medication_ID  = db.Column(db.String(10), primary_key = True)
+    Medicine_name  = db.Column(db.String(100), nullable=False)
+    Quantity = db.Column(db.INT,nullable=False)
+    Description = db.Column(db.String(100),nullable=False)
+
+    def __init__(self, Medication_ID, Medicine_name, Quantity, Description):
+        self.Medication_ID = Medication_ID
+        self.Medicine_name = Medicine_name
+        self.Quantity = Quantity
+        self.Description = Description
+
+    def json(self):
+        return {"Medication ID": self.Medication_ID,
+                "Medicine Name": self.Medicine_name,
+                "Quantity": self.Quantity,
+                "Description": self.Description
+                }
+                               
+@app.route("/medicine")
+def receive_medication_list():
+    return jsonify({"Medicine": [medicine.json() for medicine in Medication.query.all()]})
+
+if __name__ =='__main__': #It will run as a flask app if it is being imported.
+    app.run(port=6969,debug=True)
