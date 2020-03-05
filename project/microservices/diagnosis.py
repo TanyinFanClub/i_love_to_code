d44f552fda77010ad94e10485ebd1c58e88d4af2
tanyin-smu <54673611+tanyin-smu@users.noreply.github.com>
2020-03-05 16:55:13 +0800

    Updated Diagnosis.py
    Diagnosis.py debugged for problems faced with database

--- project/microservices/diagnosis.py
+++ project/microservices/diagnosis.py
@@ -13,17 +13,17 @@ CORS(app)
 class Diagnosis (db.Model):
     __tablename__ = 'diagnosis'
 
-    QueueID = db.Column(db.String(10), primary_key=True),
-    Registration_time = db.Column(db.DateTime, primary_key=True),
-    NRIC = db.Column(db.String(10), nullable=False),
-    Initial_diagnosis = db.Column(db.String(100)),
-    Doctor_diagnosis = db.Column(db.String(100)),
-    Hospital = db.Column(db.String(50), nullable=False),
-    Nurse_IC = db.Column(db.String(20), nullable=False),
-    Doctor_IC = db.Column(db.String(20), nullable=False),
-    Nurse_cleared = db.Column(db.Boolean, nullable=False),
-    Severe = db.Column(db.Boolean),
-    Complete = db.Column(db.Boolean, nullable=False),
+    QueueID = db.Column(db.String(10), primary_key=True)
+    Registration_time = db.Column(db.DateTime, primary_key=True)
+    NRIC = db.Column(db.String(10), nullable=False)
+    Initial_diagnosis = db.Column(db.String(100))
+    Doctor_diagnosis = db.Column(db.String(100))
+    Hospital = db.Column(db.String(50), nullable=False)
+    Nurse_IC = db.Column(db.String(20), nullable=False)
+    Doctor_IC = db.Column(db.String(20), nullable=False)
+    Nurse_cleared = db.Column(db.Boolean, nullable=False)
+    Severe = db.Column(db.Boolean)
+    Complete = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, QueueID, Registration_time, NRIC, Initial_diagnosis, Doctor_diagnosis, Hospital, Nurse_IC, Doctor_IC, Nurse_cleared, Severe, Complete):
         self.QueueID = QueueID
@@ -39,14 +39,22 @@ class Diagnosis (db.Model):
         self.Complete = Complete
 
     def json(self):
-        return {"QueueID": self.QueueID, "Registration_time": self.Registration_time, "NRIC": self.NRIC, "Initial_diagnosis": self.Initial_diagnosis,
-        "Doctor_diagnosis": self.Doctor_diagnosis, "Hospital": self.Hospital, "Nurse_IC": self.Nurse_IC, "Doctor_IC": self.Doctor_IC,
-        "Nurse_cleared": self.Nurse_cleared, "Severe": self.Severe, "Complete": self.Complete}
+        return {"QueueID": self.QueueID, 
+                "Registration_time": self.Registration_time, 
+                "NRIC": self.NRIC, 
+                "Initial_diagnosis": self.Initial_diagnosis,
+                "Doctor_diagnosis": self.Doctor_diagnosis, 
+                "Hospital": self.Hospital, 
+                "Nurse_IC": self.Nurse_IC, 
+                "Doctor_IC": self.Doctor_IC,
+                "Nurse_cleared": self.Nurse_cleared, 
+                "Severe": self.Severe, 
+                "Complete": self.Complete}
 
 
 @app.route("/all")
 def get_all():
-	return jsonify({"cases": [case.json() for case in diagnosis.query.all()]})
+	return jsonify({"cases": [case.json() for case in Diagnosis.query.all()]})
 
 
 
